e8bcd14731a43856cebd4a8b20319f749e2cedc9
nevge336 <neveu.ge@gmail.com>
2023-09-28 17:39:29 -0400

    préparer les fichiers php

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-import ToDoApp from "./classes/ToDoApp.js";
+import App from "./classes/App.js";
 
 (function () {
     //initialiser le Gestionnaire de librairie
-    const app = new ToDoApp(document.querySelector("[data-librairie]"));
+    const app = new App(document.querySelector("[data-librairie]"));
 })();
